05a2ef4d641b6376924409039d9ad63fb6d6c324
Lalit Sharma <lalit172952@gmail.com>
2026-02-19 23:28:42 +0530

    done

---
+++ 01-basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=1144553
+let accountEmail="lalit@gmail.com"
+var accountPassword="123456"
+ accountCity ="Jaipur"
+ let accountstate;
+
+//accountId=2//not allowed
+
+accountEmail="hc@hc.com"
+accountPassword="2121211"
+accountCity ="Bengaluru"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate])
